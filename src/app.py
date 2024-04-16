e046c6b61c8c67ca2cf03dfa060fe1185a308b96
rafael nava <153205425+rafaelnavam@users.noreply.github.com>
2024-04-16 22:36:25 +0000

    tarea realizada

--- src/app.py
+++ src/app.py
@@ -1,44 +1,86 @@
 """
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
+Este módulo se encarga de iniciar el servidor API, cargar la base de datos y agregar los endpoints.
 """
-import os
-from flask import Flask, request, jsonify, url_for
-from flask_cors import CORS
-from utils import APIException, generate_sitemap
-from datastructures import FamilyStructure
-#from models import Person
 
-app = Flask(__name__)
-app.url_map.strict_slashes = False
-CORS(app)
+import os  # Importa el módulo os para trabajar con funcionalidades dependientes del sistema operativo
+from flask import Flask, request, jsonify, url_for  # Importa las clases necesarias de Flask para construir la API
+from flask_cors import CORS  # Importa CORS para permitir solicitudes de diferentes orígenes
+from utils import APIException, generate_sitemap  # Importa las funciones auxiliares
+from datastructures import FamilyStructure  # Importa la clase FamilyStructure del módulo datastructures
+# from models import Person  # Importa el modelo de datos de Persona si es necesario
 
-# create the jackson family object
+app = Flask(__name__)  # Crea una instancia de la aplicación Flask con el nombre del módulo actual
+app.url_map.strict_slashes = False  # Configura Flask para que las rutas no requieran una barra al final
+CORS(app)  # Configura CORS para permitir solicitudes de cualquier origen
+
+# Crea una instancia de la estructura de datos de la familia Jackson
 jackson_family = FamilyStructure("Jackson")
 
-# Handle/serialize errors like a JSON object
+# Maneja y serializa errores como un objeto JSON
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
-    return jsonify(error.to_dict()), error.status_code
+    return jsonify(error.to_dict()), error.status_code  # Retorna el error como un JSON con el código de estado correspondiente
 
-# generate sitemap with all your endpoints
+# Genera el sitemap con todos los endpoints disponibles en la aplicación
 @app.route('/')
 def sitemap():
-    return generate_sitemap(app)
+    return generate_sitemap(app)  # Utiliza la función generate_sitemap del módulo utils para generar el sitemap
 
+#----------------------------------------------------------GET ALL----------------------------------------------------------------#
+
+# Endpoint para obtener todos los miembros de la familia
 @app.route('/members', methods=['GET'])
-def handle_hello():
-
-    # this is how you can use the Family datastructure by calling its methods
-    members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+def get_all_members():
+    members = jackson_family.get_all_members()  # Obtiene todos los miembros de la familia
+    if not members:
+        return jsonify({"done": False, "msg":"Error al obtener el miembro"}), 400  # Retorna un mensaje de error si falla con el código de estado 400, "done" es una convención que se utiliza comúnmente para indicar que una tarea o una operación se ha completado o realizado con éxito
+    return jsonify(members), 200  # Retorna el miembro si tiene éxito con el código de estado 200
 
 
-    return jsonify(response_body), 200
+#-----------------------------------------------------------POST---------------------------------------------------------------#
 
-# this only runs if `$ python src/app.py` is executed
+# Endpoint para agregar un nuevo miembro a la familia
+@app.route('/member', methods=['POST'])
+def add_member():
+    new_member = request.json  # Obtiene los datos del nuevo miembro desde la solicitud HTTP
+    jackson_family.add_member(new_member)  # Agrega el nuevo miembro a la familia
+    return jsonify({"done": True, "msg": "Usuario creado satisfactoriamente"}), 200  # Retorna un mensaje de éxito como JSON con el código de estado 200, "done" es una convención que se utiliza comúnmente para indicar que una tarea o una operación se ha completado o realizado con éxito
+                                                                                     
+#-----------------------------------------------------------DELETE---------------------------------------------------------------#
+
+# Endpoint para eliminar un miembro de la familia según su ID
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def delete_member(member_id):
+    eliminar_miembro = jackson_family.delete_member(member_id)  # Elimina el miembro de la familia
+    if not eliminar_miembro:
+        return jsonify({"done": False, "msg":"Error al eliminar el miembro"}), 400  # Retorna un mensaje de error si falla con el código de estado 400
+    return jsonify({"done": True, "msg":"Se ha eliminado satisfactoriamente"}), 200  # Retorna un mensaje de éxito si tiene éxito con el código de estado 200, "done" es una convención que se utiliza comúnmente para indicar que una tarea o una operación se ha completado o realizado con éxito
+
+#--------------------------------------------------------PUT------------------------------------------------------------------#
+
+# Endpoint para actualizar un miembro de la familia según su ID
+@app.route('/member/<int:member_id>', methods=['PUT'])
+def update_member(member_id):
+    new_member = request.json  # Obtiene los datos del miembro actualizado desde la solicitud HTTP
+    update_member = jackson_family.update_member(member_id, new_member)  # Actualiza el miembro de la familia
+    if not update_member:
+        return jsonify({"done": False, "msg":"Error al actualizar el miembro"}), 400  # Retorna un mensaje de error si falla con el código de estado 400
+    return jsonify({"done": True, "msg":"Se ha actualizado satisfactoriamente"}), 200  # Retorna un mensaje de éxito si tiene éxito con el código de estado 200, "done" es una convención que se utiliza comúnmente para indicar que una tarea o una operación se ha completado o realizado con éxito
+
+#------------------------------------------------------GET ONE--------------------------------------------------------------------#
+
+# Endpoint para obtener un miembro de la familia según su ID
+@app.route('/member/<int:member_id>', methods=['GET'])
+def get_member(member_id):
+    miembro_buscado = jackson_family.get_member(member_id)  # Busca el miembro en la familia
+    if not miembro_buscado:
+        return jsonify({"done": False, "msg":"Error al obtener el miembro"}), 400  # Retorna un mensaje de error si falla con el código de estado 400
+    return jsonify(miembro_buscado), 200  # Retorna el miembro si tiene éxito con el código de estado 200
+
+
+#--------------------------------------------------------------------------------------------------------------------------#
+
+# Este bloque se ejecuta solo si este script es ejecutado directamente
 if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 3000))
-    app.run(host='0.0.0.0', port=PORT, debug=True)
+    PORT = int(os.environ.get('PORT', 3000))  # Obtiene el número de puerto del entorno, predeterminado es 3000
+    app.run(host='0.0.0.0', port=PORT, debug=True)  # Inicia la aplicación Flask en el host y puerto especificados con modo de depuración activado
--- src/datastructures.py
+++ src/datastructures.py
@@ -1,36 +1,74 @@
+"""
+Actualiza este archivo para implementar los siguientes métodos ya declarados:
+- add_member: Debería agregar un miembro a la lista self._members
+- delete_member: Debería eliminar un miembro de la lista self._members
+- update_member: Debería actualizar un miembro de la lista self._members
+- get_member: Debería devolver un miembro de la lista self._members
+"""
 
-"""
-update this file to implement the following already declared methods:
-- add_member: Should add a member to the self._members list
-- delete_member: Should delete a member from the self._members list
-- update_member: Should update a member from the self._members list
-- get_member: Should return a member from the self._members list
-"""
 from random import randint
 
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
 
-        # example list of members
-        self._members = []
+        # Lista de ejemplo de miembros
+        self._members = [
+            {   "id": 1,
+                "first_name": "john",
+                "age": 33,
+                "lucky_numbers": [7, 13, 22]
+            },
+            {
+                "id": 2,
+                "first_name": "jane",
+                "age": 35,
+                "lucky_numbers": [10, 14, 3]
+            },
+            {
+                "id": 3,
+                "first_name": "jimmy",
+                "age": 5,
+                "lucky_numbers": [7]
+            }
+        ]
 
-    # read-only: Use this method to generate random members ID's when adding members into the list
+    # Solo lectura: Usa este método para generar IDs aleatorios para los miembros cuando se agregan a la lista
     def _generateId(self):
         return randint(0, 99999999)
 
+    # Método para agregar un miembro a la lista
     def add_member(self, member):
-        # fill this method and update the return
-        pass
+        # Si el miembro no tiene un ID, se genera uno aleatoriamente
+        if not member.get("id"):
+            member["id"] = self._generateId()  # Genera un ID aleatorio si no hay uno proporcionado
+        self._members.append(member)  # Agrega el miembro a la lista de miembros
 
+    # Método para eliminar un miembro de la lista según su ID
     def delete_member(self, id):
-        # fill this method and update the return
-        pass
+        for member in self._members:
+            if member["id"] == id:
+                self._members.remove(member)  # Elimina el miembro de la lista
+                return True  # Retorna Verdadero indicando que se encontró y eliminó el miembro
+        return False  # Retorna Falso si no se encontró el miembro con el ID dado
 
+    # Método para actualizar un miembro de la lista según su ID
+    def update_member(self, id, member):
+        for family_member in self._members:
+            if family_member["id"] == id:
+                self._members.remove(family_member)  # Elimina el miembro existente
+                member["id"] = id  # Actualiza el ID del miembro nuevo o modificado
+                self._members.append(member)  # Agrega el miembro actualizado a la lista
+                return True  # Retorna Verdadero indicando que se actualizó el miembro
+        return False  # Retorna Falso si no se encontró el miembro con el ID dado
+
+    # Método para obtener un miembro de la lista según su ID
     def get_member(self, id):
-        # fill this method and update the return
-        pass
+        for family_member in self._members:
+            if family_member["id"] == id:
+                return family_member  # Retorna el miembro si se encuentra en la lista
+        return False  # Retorna Falso si no se encuentra el miembro con el ID dado
 
-    # this method is done, it returns a list with all the family members
+    # Este método devuelve una lista con todos los miembros de la familia
     def get_all_members(self):
-        return self._members
+        return self._members  # Retorna la lista completa de miembros
